a41b8ccb1297cecc84c213452ad5a9461dbbb9f5
Oscar Javier Garcia Baudet <oscarjavier.garcia@rtve.es>
2023-05-18 18:13:40 +0200

    Added support for several additional operations

--- checks.py
+++ checks.py
@@ -8,14 +8,19 @@ parser = argparse.ArgumentParser(
     description="Checks for library functionality",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("path", help="Path to the '.dbs' directory")
-parser.add_argument("database", help="Dump database data")
-parser.add_argument("--schema", action="store_true", default=False,
-                    help="Include schema dump")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("--list-tables", action="store_true",
+                   help="List tables in database")
+group.add_argument("--dump-schema", action="store_true",
+                   help="Dump schema data")
+group.add_argument("--dump-table", metavar="table_name", help="Dump database data")
 args = parser.parse_args()
 config = vars(args)
-
 database = MultibaseReader(config["path"])
 
-if config["schema"]:
-    print(json.dumps(database.schema[config["database"]], indent=4))
-print(json.dumps(database.read_table(config["database"]), indent=4))
+if config["dump_schema"]:
+    print(json.dumps(database.schema, indent=4))
+elif config["list_tables"]:
+    print(json.dumps(list(database.schema.keys()), indent=4))
+elif config["dump_table"]:
+    print(json.dumps(database.read_table(config["dump_table"]), indent=4))
